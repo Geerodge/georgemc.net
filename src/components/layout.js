8b138ae7917598951c9dca764f50d5b97f73d38a
George McEntegart <geerodge@gmail.com>
2021-02-02 19:46:00 +0000

    css grid
    added css grid framework

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,26 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import GlobalStyles from "../styles/GlobalStyles";
 import Header from "./header"
+import styled from "styled-components";
+
+const LayoutStyles = styled.div`
+  .grid-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    gap: 0px 0px;
+    grid-template-areas:
+      "header header header header"
+      ". content content ."
+      "footer footer footer footer";
+  }
+
+  .header { grid-area: header; }
+
+  .footer { grid-area: footer; }
+
+  .content { grid-area: content; }
+`;
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,13 +44,21 @@ const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyles />
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()} George McEntegart
-        </footer>
+      <LayoutStyles>
+      <div class="grid-container">
+        <div class="header">
+          <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+        </div>
+        <div class="content">
+          <main>{children}</main>
+        </div>
+        <div class="footer">
+          <footer>
+            © {new Date().getFullYear()} George McEntegart
+          </footer>
+        </div>
       </div>
+      </LayoutStyles>
     </>
   )
 }
@@ -40,3 +68,5 @@ Layout.propTypes = {
 }
 
 export default Layout
+
+
