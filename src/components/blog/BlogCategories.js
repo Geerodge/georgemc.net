7f21a79e0f3d3b7d1a939d8b6f8d9d1978000ccd
George McEntegart <geerodge@gmail.com>
2021-07-26 23:07:16 +0100

    Update BlogCategories.js

--- src/components/blog/BlogCategories.js
+++ src/components/blog/BlogCategories.js
@@ -5,16 +5,22 @@ const CategoryStyles = styled.div`
 
 `;
 
+
 const BlogCategories = (props) => (
 
     <CategoryStyles>
         <p>
-            {props.categories.map((category, index) => (
-                <span key={index}>{category.title + ` `}</span>
-            ))}
+            {props.categories.map((category, i, index) => {
+                const lastIndex = props.categories.length - 1;
+                if (i === lastIndex) {
+                    return (<span key={index}>{category.title}</span>)
+                } else {
+                    return (<span key={index}>{category.title + `, `}</span>)
+                }
+            })}
         </p>
     </CategoryStyles>
 
-)
+);
 
 export default BlogCategories;
