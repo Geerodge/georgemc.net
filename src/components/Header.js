f2dce9fac51b0e513488c78fc9cb4e0a71e714c6
George McEntegart <geerodge@gmail.com>
2021-07-26 20:41:53 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -75,7 +75,7 @@ const HeaderStyles = styled.div`
         background: var(--primary);
         z-index: 2;
         visibility: hidden;
-        position: fixed;
+        position: absolute;
         &.active {
             visibility: visible;
         }
