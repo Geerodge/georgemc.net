ce60284078bd825b14c392b4315d4ca3d5433eb7
George McEntegart <geerodge@gmail.com>
2021-07-29 22:23:13 +0100

    Update BlogPosts.js

--- src/templates/BlogPosts.js
+++ src/templates/BlogPosts.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 import BlockContent from "@sanity/block-content-to-react";
 import styled from "styled-components";
 import Layout from "../components/Layout";
@@ -24,91 +24,87 @@ export default function BlogPosts({ pageContext: { slug }, data: { allSanityPost
     // Deconstruct page data
     const blogData = allSanityPost.edges[0].node;
 
-    return (
-        <StaticQuery
-          query={graphql`
-          query($slug: String!) {
-            allSanityPost(filter: {slug: {current: {eq: $slug}}}) {
-                edges {
-                    node {
-                      id
-                      title
-                      seoTitle
-                      seoDescription
-                      _rawBody
-                      _updatedAt
-                      _createdAt
-                      author {
-                        name
-                        mainImage {
-                          alt
-                          asset {
-                            fixed(width: 400) {
-                              ...GatsbySanityImageFixed
-                            }
-                          }
-                        }
-                        bio {
-                          _key
-                          _type
-                          style
-                          list
-                          _rawChildren
+    const dataNew = useStaticQuery(graphql`
+      query($slug: String!) {
+        allSanityPost(filter: {slug: {current: {eq: $slug}}}) {
+            edges {
+                node {
+                  id
+                  title
+                  seoTitle
+                  seoDescription
+                  _rawBody
+                  _updatedAt
+                  _createdAt
+                  author {
+                    name
+                    mainImage {
+                      alt
+                      asset {
+                        fixed(width: 400) {
+                          ...GatsbySanityImageFixed
                         }
                       }
-                      categories {
-                        title
-                      }
-                      mainImage {
-                        alt
-                        asset {
-                          fluid(maxWidth: 1000) {
-                            ...GatsbySanityImageFluid
-                          }
-                        }
-                      }
-                      body {
-                        _key
-                        _type
-                        style
-                        list
-                        _rawChildren
+                    }
+                    bio {
+                      _key
+                      _type
+                      style
+                      list
+                      _rawChildren
+                    }
+                  }
+                  categories {
+                    title
+                  }
+                  mainImage {
+                    alt
+                    asset {
+                      fluid(maxWidth: 1000) {
+                        ...GatsbySanityImageFluid
                       }
                     }
                   }
+                  body {
+                    _key
+                    _type
+                    style
+                    list
+                    _rawChildren
+                  }
                 }
               }
-          `}
-          render={blogData => (
-            <Layout>
-              <BlogStyles>
-                <Seo 
-                  title={blogData.seoTitle}
-                  description={blogData.seoDescription}
-                />
-                <h1>{blogData.title}</h1>
-                <BlogDate 
-                  createdAt={blogData._createdAt}
-                  updatedAt={blogData._updatedAt}
-                />
-                <BlogImage 
-                  data={blogData}
-                />
-                <BlockContent
-                  blocks={blogData._rawBody}
-                />
-                <BlogCategories
-                  categories={blogData.categories}
-                />
-                <BlogAuthor 
-                  author={blogData.author.name}
-                  src={blogData.author.mainImage.asset.fixed}
-                  alt={blogData.author.mainImage.alt}
-                  bio={blogData.author.bio[0]._rawChildren[0].text}
-                />
-              </BlogStyles>
-            </Layout>
-          )}
-        />
+            }
+          }
+    `)
+    return (
+      <Layout>
+        <BlogStyles>
+          <Seo 
+            title={blogData.seoTitle}
+            description={blogData.seoDescription}
+          />
+          <h1>{blogData.title}</h1>
+          <BlogDate 
+            createdAt={blogData._createdAt}
+            updatedAt={blogData._updatedAt}
+          />
+          <BlogImage 
+            data={blogData}
+          />
+          <BlockContent
+            blocks={blogData._rawBody}
+          />
+          <BlogCategories
+            categories={blogData.categories}
+          />
+          <BlogAuthor 
+            author={blogData.author.name}
+            src={blogData.author.mainImage.asset.fixed}
+            alt={blogData.author.mainImage.alt}
+            bio={blogData.author.bio[0]._rawChildren[0].text}
+          />
+        </BlogStyles>
+      </Layout>
     )
 }
