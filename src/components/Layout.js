c68e854dff6127c2d3b037ba0906601725a86232
George McEntegart <geerodge@gmail.com>
2021-08-05 18:51:20 +0100

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -46,7 +46,7 @@ const LayoutStyles = styled.div`
 const Layout = ({ children }) => {
 
     // Check if window is defined (so if in the browser or in node.js).
-    const isBrowser = typeof window !== "undefined"
+    const isBrowser = typeof window !== "undefined";
 
     // Check if it's the homepage to display Hero component
     function isHome() {
