c8afd3fd3227174559fadd83c963ca6aea02c7af
George McEntegart <geerodge@gmail.com>
2021-08-05 18:36:07 +0100

    Updated class to className

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,6 @@ import Seo from "../components/Seo";
 import ContactForm from "../components/ContactForm";
 import styled from "styled-components";
 
-
 const ContactPageStyles = styled.div`
 
     margin: 2em 1em;
